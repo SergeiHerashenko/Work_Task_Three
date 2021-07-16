fe1815f4c7765c87e6cd34aebde3141c68c64dc2
SergeiHerashenko <46726784+SergeiHerashenko@users.noreply.github.com>
2021-07-16 17:51:49 +0300

    Task-Three

---
+++ Task_Three/Task_Three/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Перший варіант.
+            string Stroka_One = null;
+            int i = Stroka_One.Length;
+
+            //Другий варіант
+            string Stroka_Two = null;
+            Stroka_Two = Stroka_Two.ToUpper();
+
+            //Помилка NullReferenceException іиникає в результаті того що ми намагались отримати доступ до члену, який нічого не має
+            //Тобто змінна була не неініціалізована, або була неініціалізована , а зараз не неініціалізована.
+        }
+    }
+}
